9dfe4c4078fe2f62764ffd28bbf645a0305e3846
YingKuan <kuan79223@gmail.com>
2023-07-13 17:24:09 +0800

    create UI python

--- main.py
+++ main.py
@@ -4,203 +4,168 @@ import sys
 import time
 import numpy as np
 import cv2
-from PyQt5 import QtWidgets
+import threading
 from PyQt5.QtCore import QThread, pyqtSignal, pyqtSlot, Qt
 from PyQt5.QtGui import QImage, QPixmap
-from PyQt5.QtWidgets import QVBoxLayout, QPushButton, QHBoxLayout, QLineEdit, \
-    QFileDialog, QGridLayout, QLabel
+from PyQt5.QtWidgets import QFileDialog, QMainWindow, QGraphicsScene, QApplication
+
+import camera_ui
+
 
 WIDTH = 1920
 HEIGHT = 1080
-THRESH = 128
-MAXVAL = 255
 COM = 0
+
 VIDEO = cv2.VideoCapture(COM, cv2.CAP_DSHOW)
 
-# 影像處理狀態切換
-PROCESS_STATUS = 'default'
 
-
+# 即時顯示影像與處理影像
 class CameraThread(QThread):
-    SIGNAL_FRAME = pyqtSignal(QImage)
+    SIGNAL_FRAME = pyqtSignal(np.ndarray)
     SIGNAL_PROCESS_IMAGE = pyqtSignal(QImage)
-    process = ''
 
     def __init__(self, main):
         super(CameraThread, self).__init__()
         self.main = main
         self.running = True
         self.isDetectCamera = True
+        self.frame = None  # 用於處理影像
+        # initialize binary, dilate, erode, guass value
+        self.binary_value = 0
+        # kernel value
+        self.dilate_value = 0
+        self.erode_value = 0
+
+        self.blur_value = 1
+        self.binary_frame = None  # 預設二值化變數
 
     def run(self):
         set_pixels(VIDEO)  # 設定像素
         while self.running:
-
-            time.sleep(0.000001)
+            time.sleep(0.001)
             if VIDEO.isOpened() is True:
-                ret, frame = VIDEO.read()
+                ret, frame = VIDEO.read()  # 讀取影像回傳 bool 與 影像，bool 代表相機有讀取到影像
+                self.frame = frame
+
                 if ret:
-                    height, width = frame.shape[:2]
-                    image = QImage(bytes(frame), width, height, 3 * width, QImage.Format_BGR888)
-                    self.SIGNAL_FRAME.emit(image)
+                    self.SIGNAL_FRAME.emit(frame)  # 傳遞原圖影像信號
 
-                    kernel_dilate = np.ones((5, 5), np.uint8)  # 膨脹 kernel
-                    kernel_erode = np.ones((5, 5), np.uint8)  # 侵蝕 kernel
+                    # 影像處理之前會先做二值化
+                    gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+                    _, self.binary_frame = cv2.threshold(gray, self.binary_value, 255, cv2.THRESH_BINARY)
 
-                    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                    _, binary_frame = cv2.threshold(gray, THRESH, MAXVAL, cv2.THRESH_BINARY)
+                    img_process = cv2.GaussianBlur(self.binary_frame, (self.blur_value, self.blur_value), 10)
 
-                    # 二值化
-                    if self.process == 'btn_binary':
-                        height, width = binary_frame.shape[:2]
-                        binary_image = QImage(bytes(binary_frame), width, height, QImage.Format_Grayscale8)
-                        self.SIGNAL_PROCESS_IMAGE.emit(binary_image)
-                    # 高斯模糊
-                    elif self.process == 'btn_gaussian_blur':
-                        blur = cv2.GaussianBlur(frame, (15, 15), 10)
-                        height, width = blur.shape[:2]
-                        blur_image = QImage(bytes(blur), width, height, QImage.Format_Grayscale8)
-                        self.SIGNAL_PROCESS_IMAGE.emit(blur_image)
-                    # 膨脹
-                    elif self.process == 'btn_dilate':
-                        dilate = cv2.dilate(binary_frame, kernel_dilate, iterations=10)
-                        height, width = dilate.shape[:2]
-                        dilate_image = QImage(bytes(dilate), width, height, QImage.Format_Grayscale8)
-                        self.SIGNAL_PROCESS_IMAGE.emit(dilate_image)
-                    # 侵蝕
-                    elif self.process == 'btn_erode':
-                        erode = cv2.erode(binary_frame, kernel_erode, iterations=1)  # 侵蝕次數
-                        height, width = erode.shape[:2]
-                        erode_image = QImage(bytes(erode), width, height, QImage.Format_Grayscale8)
-                        self.SIGNAL_PROCESS_IMAGE.emit(erode_image)
+                    kernel_dilate = np.ones((self.dilate_value, self.dilate_value), np.uint8)  # 膨脹 kernel
+                    img_process = cv2.dilate(img_process, kernel_dilate, iterations=1)
+
+                    kernel_erode = np.ones((self.erode_value, self.erode_value), np.uint8)  # 侵蝕 kernel
+                    img_process = cv2.erode(img_process, kernel_erode, iterations=1)
+
+                    height, width = img_process.shape[:2]
+                    # 假如影像有處理就以 2 通道來顯示，否則就以原圖 3 通道呈現
+                    if len(img_process.shape) == 2:
+                        bytesPerline = width
+                        qimg = QImage(img_process.data, width, height, bytesPerline, QImage.Format_Grayscale8)
+                    else:
+                        bytesPerline = width * 3
+                        qimg = QImage(img_process.data, width, height, bytesPerline, QImage.Format_BGR888)
+
+                    self.SIGNAL_PROCESS_IMAGE.emit(qimg)  # 傳遞處理後的影像信號
+
+    # slider bar 觸發連接
+    def update_process(self):
+        object_name = self.sender().objectName()
+
+        if object_name == 'slider_binary':
+            self.binary_value = self.sender().value()
+            self.main.label_binary.setText(str(self.binary_value))
+
+        elif object_name == 'slider_dilate':
+            self.dilate_value = self.sender().value()
+            self.main.label_dilate.setText(str(self.dilate_value))
+
+        elif object_name == 'slider_erode':
+            self.erode_value = self.sender().value()
+            self.main.label_erode.setText(str(self.erode_value))
+
+        elif object_name == 'spinBox_guass':
+            self.blur_value = self.sender().value()
+
     def stop(self):
         self.running = False
         VIDEO.release()
         sys.exit()
 
-class CamaraCapture(QtWidgets.QWidget):
+
+class CamaraCapture(QMainWindow, camera_ui.Ui_MainWindow):
 
     def __init__(self):
         super(CamaraCapture, self).__init__()
-        self.setFixedSize(1024, 768) # 設定介面大小
+        self.setupUi(self)
+
+        self.setFixedSize(1280, 768)  # 設定介面大小
         self.setWindowTitle('CamaraCapture')  # 介面標題
         self.setWindowFlags(Qt.FramelessWindowHint)
-        layout = QVBoxLayout()  # 主視窗布局
 
-        view_layout = QHBoxLayout()
-        self.origin_label = QLabel()
-        self.origin_label.setScaledContents(True)  # 畫面自適應label
-        view_layout.addWidget(self.origin_label)
+        self.lineEdit.textEdited.connect(self.validate_path)
+        self.btn_select_path.clicked.connect(self.open_dialog)  # 開啟選擇路徑的信號連接
+        self.open_dialog_thread = None
 
-        self.process_label = QLabel()
-        self.process_label.setScaledContents(True)  # 畫面自適應label
-        view_layout.addWidget(self.process_label)
+        self.btn_capture.clicked.connect(self.capture)
+        self.btn_close_sys.clicked.connect(self.close_sys)
 
-        layout.addLayout(view_layout)
-
-        path_layout = QHBoxLayout()  # 選擇路徑與顯示路徑的布局
-
-        self.path_edit = QLineEdit()
-        self.path_edit.setFixedSize(600, 50)
-        font = self.path_edit.font()
-        font.setPointSize(16)
-        self.path_edit.setFont(font)
-        self.path_edit.textEdited.connect(self.validate_path)
-        path_layout.addWidget(self.path_edit)
-
-        # 選擇儲存路徑的按鈕設定與事件
-        select_btn = QPushButton('路徑')
-        select_btn.setFixedSize(100, 50)
-        path_layout.addWidget(select_btn)
-        select_btn.clicked.connect(self.select_save_path)
-
-        grid_layout = QGridLayout()  # 使用網格布局按鈕
-        # --------- 按鈕設定與觸發事件 -------------
-        btn_binary = QPushButton('二值化')
-        btn_binary.setFixedSize(100, 50)
-        btn_binary.clicked.connect(lambda: self.button_clicked('btn_binary'))
-
-        btn_gaussian_blur = QPushButton('高斯模糊')
-        btn_gaussian_blur.setFixedSize(100, 50)
-        btn_gaussian_blur.clicked.connect(lambda: self.button_clicked('btn_gaussian_blur'))
-
-        btn_dilate = QPushButton('膨脹')
-        btn_dilate.setFixedSize(100, 50)
-        btn_dilate.clicked.connect(lambda: self.button_clicked('btn_dilate'))
-
-        btn_erode = QPushButton('侵蝕')
-        btn_erode.setFixedSize(100, 50)
-        btn_erode.clicked.connect(lambda: self.button_clicked('btn_erode'))
-
-        btn_capture = QPushButton('拍照')
-        btn_capture.setFixedSize(100, 50)
-        btn_capture.clicked.connect(self.capture)
-
-        btn_leave = QPushButton('關閉應用程式')
-        btn_leave.setFixedSize(100, 50)
-        btn_leave.clicked.connect(self.leave_sys)
-        # 按鈕佈進網格內
-        grid_layout.addWidget(btn_binary, 0, 1)
-        grid_layout.addWidget(btn_gaussian_blur, 0, 2)
-        grid_layout.addWidget(btn_dilate, 0, 3)
-        grid_layout.addWidget(btn_erode, 0, 4)
-        grid_layout.addWidget(btn_capture, 1, 1)
-        grid_layout.addWidget(btn_leave, 1, 2)
         self.frame = None
         self.save_path = ''
-
         # 打開應用程式就啟動執行緒捕捉畫面
         self.camera_thread = CameraThread(self)
-        self.camera_thread.SIGNAL_FRAME.connect(self.display_frame)
-        self.camera_thread.SIGNAL_PROCESS_IMAGE.connect(self.process_image)
+        self.camera_thread.SIGNAL_FRAME.connect(self.display_video)
+        self.camera_thread.SIGNAL_PROCESS_IMAGE.connect(self.display_process_video)
         self.camera_thread.start()
 
-        layout.addLayout(path_layout)
-        layout.addLayout(grid_layout)
-        self.setLayout(layout)
+        self.slider_binary.valueChanged.connect(self.camera_thread.update_process)
+        self.slider_dilate.valueChanged.connect(self.camera_thread.update_process)
+        self.slider_erode.valueChanged.connect(self.camera_thread.update_process)
 
-    def button_clicked(self, flag):
-        sender_name = self.sender().objectName()
-        # print(sender_name)
-        # self.process_thread.process = sender_name
-        # global PROCESS_STATUS
-        # 接收按鈕觸發的狀態
-        if flag == 'btn_binary':
-            self.camera_thread.process = 'btn_binary'
-        elif flag == 'btn_gaussian_blur':
-            self.camera_thread.process = 'btn_gaussian_blur'
-        elif flag == 'btn_dilate':
-            self.camera_thread.process = 'btn_dilate'
-        elif flag == 'btn_erode':
-            self.camera_thread.process = 'btn_erode'
+        self.spinBox_guass.valueChanged.connect(self.camera_thread.update_process)
 
     @pyqtSlot(QImage)  # 處理影像執行緒的信號槽
-    def process_image(self, process_frame):
-        self.process_label.setPixmap(QPixmap.fromImage(process_frame))
+    def display_process_video(self, qimg):
+        scene_processed = QGraphicsScene()
+        scene_processed.addPixmap(QPixmap(qimg))
+        self.graph_process.setScene(scene_processed)
+        # 讓場景自適應視圖 兩個與法都可以用，第一個參數代表 場景的邊界框
+        self.graph_process.fitInView(scene_processed.sceneRect(), Qt.KeepAspectRatio)
+
+    def open_dialog(self):
+        self.open_dialog_thread = threading.Thread(target=self.show_dialog())
+        self.open_dialog_thread.start()
 
     # 選擇儲放影像的路徑
-    def select_save_path(self):
-
-        self.save_path = QFileDialog.getExistingDirectory(self, '選擇儲存路徑', 'D:')
-
-        if self.save_path == '':
-            print('\n取消選擇')
-            return
-        else:
-            self.path_edit.setText(self.save_path)
+    def show_dialog(self):
+        path = QFileDialog.getExistingDirectory(self, '選擇儲存路徑', 'D:')
+        print(path)
+        if path:
+            self.lineEdit.setText(path)
 
     # 判斷 line edit 是否存在字串內容
     def validate_path(self):
-        text = self.path_edit.text()
+        text = self.lineEdit.text()
         if text:
             self.save_path = text
         else:
             self.save_path = ''
 
-    @pyqtSlot(QImage)  # 顯示影像執行緒的信號槽
-    def display_frame(self, frame):
+    @pyqtSlot(np.ndarray)  # 顯示影像執行緒的信號槽
+    def display_video(self, frame):
         self.frame = frame
-        self.origin_label.setPixmap(QPixmap.fromImage(frame))
+        height, width = frame.shape[:2]
+        qimg = QImage(bytes(frame), width, height, 3 * width, QImage.Format_BGR888)
+        scene_origin = QGraphicsScene()
+        scene_origin.addPixmap(QPixmap(qimg))
+        self.graph_origin.setScene(scene_origin)
+        # 讓場景自適應視圖 兩個與法都可以用，第一個參數代表 場景的邊界框
+        self.graph_origin.fitInView(scene_origin.sceneRect(), Qt.KeepAspectRatio)
 
     # 拍照
     def capture(self):
@@ -221,13 +186,12 @@ class CamaraCapture(QtWidgets.QWidget):
             return
 
     # 離開應用程式
-    def leave_sys(self):
+    def close_sys(self):
         try:
             self.camera_thread.stop()
-            self.process_thread.stop()
+            self.open_dialog_thread.stop()
         except Exception as exc:
             print(exc)
-
 
 # 設定相機解析度
 def set_pixels(cap):
@@ -237,7 +201,7 @@ def set_pixels(cap):
 
 
 if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
-    form = CamaraCapture()
-    form.show()
+    app = QApplication(sys.argv)
+    win = CamaraCapture()
+    win.show()
     sys.exit(app.exec())
