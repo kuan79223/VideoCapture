ff608351c85af5aa0c0a14ed539508d4cc229588
YingKuan <kuan79223@gmail.com>
2023-07-21 16:54:57 +0800

    解決選擇匯入影像時發生的閃退

--- main.py
+++ main.py
@@ -16,17 +16,13 @@ WIDTH = 1920
 HEIGHT = 1080
 COM = 0
 
-IMAGE = r'D:\Python Scripts\NoGUI_run_yolov7\SHA\normal\7.jpeg'
-
 VIDEO = cv2.VideoCapture(COM, cv2.CAP_DSHOW)
 
 
 # 即時顯示影像與處理影像
 class CameraThread(QThread):
-    SIGNAL_FRAME = pyqtSignal(np.ndarray)
-    SIGNAL_PROCESS_IMAGE = pyqtSignal(QImage)
-
-    # IMAGE = IMAGE
+    SIGNAL_FRAME = pyqtSignal(np.ndarray)  # 傳遞原圖的信號
+    SIGNAL_HANDLE_IMAGE = pyqtSignal(np.ndarray, int, int)  # 傳遞處理後的影像信號
 
     def __init__(self, main):
         super(CameraThread, self).__init__()
@@ -34,13 +30,14 @@ class CameraThread(QThread):
         self.running = True
         self.isDetectCamera = True
         self.frame = None  # 處理影像的變數
-        # initialize binary, dilate, erode, guass value
+        # initialize binary, dilate, erode, gaussian value
         self.binary_value = 0
         # kernel value
         self.dilate_value = 0
         self.erode_value = 0
         self.blur_value = 1
         self.binary_frame = None  # 預設二值化變數
+        self.image_dir = ''  # 設定一個接收匯入圖片的變數
 
     def run(self):
         set_pixels(VIDEO)  # 設定像素
@@ -53,8 +50,8 @@ class CameraThread(QThread):
                 if ret:
                     self.frame = frame
                     height, width, img_process = self.handle_image()
-                    qimg = self.get_q_img(img=img_process, w=width, h=height)
-                    self.SIGNAL_PROCESS_IMAGE.emit(qimg)  # 傳遞處理後的影像信號
+
+                    self.SIGNAL_HANDLE_IMAGE.emit(img_process, height, width)  # 傳遞處理後的影像信號
 
                     contours, hierarchy = cv2.findContours(img_process, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
                     # 複製原圖避免每一次進 for loop 都會被覆蓋
@@ -74,56 +71,52 @@ class CameraThread(QThread):
                         # 左側原圖影像
                         self.SIGNAL_FRAME.emit(self.frame)
 
-            # 沒有開相機，讀取檔案圖片
             else:
-                if IMAGE:
-                    print(IMAGE)
-                    self.frame = cv2.imread(IMAGE)
-                    break
+                while self.running:
+                    time.sleep(0.001)
+                    # 判斷 讀取影像路徑的變數 和 讀取影像狀態是否成立
+                    if self.main.image_dir and self.main.isLoad_img:
+                        self.frame = cv2.imread(self.main.image_dir)
+                        # 將 讀取到的影像路徑變數放在記憶體中
+                        self.image_dir = self.main.image_dir
+                        # 因為是按鈕觸發，所以當影像路徑存到變數之後，將狀態改為 False
+                        self.main.isLoad_img = False
 
-        # 沒有相機的時候，檔案的圖像處理
-        if not VIDEO.isOpened():
-            while True:
-                time.sleep(0.1)
-                height, width, img_process = self.handle_image()
-                qimg = self.get_q_img(img=img_process, w=width, h=height)
-                self.SIGNAL_PROCESS_IMAGE.emit(qimg)  # 傳遞處理後的影像
+                    if self.frame is not None:
+                        height, width, img_process = self.handle_image()
+                        self.SIGNAL_HANDLE_IMAGE.emit(img_process, height, width)  # 傳遞處理後的影像
 
-                # 找輪廓 - 且在原圖上進行繪圖
-                contours, hierarchy = cv2.findContours(img_process, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-                # print(f'輪廓數: {len(self.contours)}')
+                        # 找輪廓 - 且在原圖上進行繪圖
+                        contours, hierarchy = cv2.findContours(img_process, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-                img_draw = self.frame.copy()
+                        img_draw = self.frame.copy()
+                        for i in range(0, len(contours)):
+                            # area = cv2.contourArea(contours[i])  # 取得輪廓面積
+                            rx_min, ry_min, rw, rh = cv2.boundingRect(contours[i])
+                            rx_max = rx_min + rw
+                            ry_max = ry_min + rh
+                            print(rx_min, ry_min, rx_max, ry_max)
 
-                for i in range(0, len(contours)):
-                    rx_min, ry_min, rw, rh = cv2.boundingRect(contours[i])
-                    rx_max = rx_min + rw
-                    ry_max = ry_min + rh
-                    # print(rx_min, ry_min, rx_max, ry_max)
+                            img_draw = cv2.rectangle(img_draw, (rx_min, ry_min), (rx_max, ry_max), (255, 0, 0), 3)
 
-                    img_draw = cv2.rectangle(img_draw, (rx_min, ry_min), (rx_max, ry_max), (255, 0, 0), 3)
-                    # height, width = img_draw.shape[:2]
-                    crop_img = self.frame[ry_min:ry_max, rx_min:rx_max]
-                    text = str(int(crop_img.mean()))
-                    cv2.putText(img_draw, text, (rx_min + int(rw / 2), ry_min + int(rh / 2)), cv2.FONT_HERSHEY_SIMPLEX,
-                                1, (255, 0, 0), 2)
+                            crop_img = self.frame[ry_min:ry_max, rx_min:rx_max]
+                            text = str(int(crop_img.mean()))  # 計算 RGB 均值
+                            # 在影像上顯示數值
+                            cv2.putText(img_draw, text, (rx_min + int(rw / 2), ry_min + int(rh / 2)), cv2.FONT_HERSHEY_SIMPLEX,
+                                        1, (0, 0, 255), 2)
 
-                    if len(contours) != 0:
-                        # 左側畫框的影像
-                        self.SIGNAL_FRAME.emit(img_draw)
-                    else:
-                        # 左側原圖影像
-                        self.SIGNAL_FRAME.emit(self.frame)
-
-    def accept_path_image(self, path):
-        self.frame = cv2.imread(path)
+                            if len(contours) != 0:
+                                # 左側畫框的影像
+                                self.SIGNAL_FRAME.emit(img_draw)
+                            else:
+                                # 左側原圖影像
+                                self.SIGNAL_FRAME.emit(self.frame)
 
     # 影像處理
     def handle_image(self):
-
         # 影像處理之前會先做二值化
         gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
-        _, self.binary_frame = cv2.threshold(gray, self.binary_value, 255, cv2.THRESH_BINARY)
+        _, self.binary_frame = cv2.threshold(gray, self.binary_value, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
         img_process = cv2.GaussianBlur(self.binary_frame, (self.blur_value, self.blur_value), 10)
 
@@ -136,18 +129,6 @@ class CameraThread(QThread):
         height, width = img_process.shape[:2]
 
         return height, width, img_process
-
-    # 判斷 影像為幾通道，給予對應的 qimg
-    def get_q_img(self, img, w, h):
-        # 假如影像有處理就以 2 通道來顯示，否則就以原圖 3 通道呈現
-        if len(img.shape) == 2:
-            bytesPerline = w
-            qimg = QImage(img.data, w, h, bytesPerline, QImage.Format_Grayscale8)
-        else:
-            bytesPerline = w * 3
-            qimg = QImage(img.data, w, h, bytesPerline, QImage.Format_BGR888)
-
-        return qimg
 
     # slider bar 觸發連接
     def update_process(self):
@@ -171,11 +152,10 @@ class CameraThread(QThread):
     def stop(self):
         self.running = False
         VIDEO.release()
-        sys.exit()
 
 
 class CamaraCapture(QMainWindow, camera_ui.Ui_MainWindow):
-    SIGNAL_UPLOAD_IMG = pyqtSignal(str)
+    SIGNAL_SHOW_DIALOG = pyqtSignal()  # 開啟對話方塊的信號
 
     def __init__(self):
         super(CamaraCapture, self).__init__()
@@ -185,15 +165,12 @@ class CamaraCapture(QMainWindow, camera_ui.Ui_MainWindow):
         self.setWindowTitle('CamaraCapture')  # 介面標題
         self.setWindowFlags(Qt.FramelessWindowHint)
 
+        self.btn_capture.clicked.connect(self.capture)
+        self.btn_close_sys.clicked.connect(self.close_sys)
+
         self.lineEdit.textEdited.connect(self.validate_path)
         self.btn_save_path.clicked.connect(self.open_select_dir_dialog)  # 開啟選擇路徑的信號連接
         self.select_dir_thread = None
-
-        # self.btn_upload.clicked.connect(self.open_upload_img_dialog)
-        self.btn_upload.clicked.connect(self.open_upload_dialog)
-
-        self.btn_capture.clicked.connect(self.capture)
-        self.btn_close_sys.clicked.connect(self.close_sys)
 
         self.frame = None
         self.save_path = ''
@@ -201,27 +178,51 @@ class CamaraCapture(QMainWindow, camera_ui.Ui_MainWindow):
         # 打開應用程式就啟動執行緒捕捉畫面
         self.camera_thread = CameraThread(self)
         self.camera_thread.SIGNAL_FRAME.connect(self.display_video)
-        self.camera_thread.SIGNAL_PROCESS_IMAGE.connect(self.display_process_video)
+        self.camera_thread.SIGNAL_HANDLE_IMAGE.connect(self.display_process_video)
         self.camera_thread.start()
-
+        # Slider Bar 變動的連接
         self.slider_binary.valueChanged.connect(self.camera_thread.update_process)
         self.slider_dilate.valueChanged.connect(self.camera_thread.update_process)
         self.slider_erode.valueChanged.connect(self.camera_thread.update_process)
-
+        # SpinBox 變動的連接
         self.spinBox_guass.valueChanged.connect(self.camera_thread.update_process)
 
-        self.open_upload_thread = None
-        self.SIGNAL_UPLOAD_IMG.connect(self.camera_thread.accept_path_image)
+        self.btn_upload.clicked.connect(self.open_upload_dialog)
+        self.SIGNAL_SHOW_DIALOG.connect(self.show_dialog)  # 傳遞開啟對話視窗的連接
 
-    @pyqtSlot(QImage)  # 處理影像執行緒的信號槽
-    def display_process_video(self, qimg):
-        scene_processed = QGraphicsScene()
-        scene_processed.addPixmap(QPixmap(qimg))
-        self.graph_process.setScene(scene_processed)
-        # 讓場景自適應視圖 兩個與法都可以用，第一個參數代表 場景的邊界框
-        self.graph_process.fitInView(scene_processed.sceneRect(), Qt.KeepAspectRatio)
+        self.scene_origin = QGraphicsScene()  # 原始畫面
+        self.scene_adjusting = QGraphicsScene()  # 調整的畫面
 
+        self.image_dir = None  # 讀取圖片路徑的變數
+        self.isLoad_img = False  # 讀取圖片的狀態
+
+    # 匯入圖片按鈕觸發傳遞信號
+    def open_upload_dialog(self):
+
+        self.SIGNAL_SHOW_DIALOG.emit()
+
+    # 彈出匯入圖片的對話視窗
+    def show_dialog(self):
+        # 相機開啟，匯入按鈕不能動作
+        if VIDEO.isOpened() is not True:
+            self.image_dir, _ = QFileDialog.getOpenFileName(self, '載入圖像', 'D:', "Image Files (*.jpg *.jpeg *.png)")  # 設置文件擴展名過濾,用雙分號間隔
+            # 選擇到影像路徑之後，改變狀態
+            if self.image_dir:
+                self.isLoad_img = True
+        else:
+            self.btn_upload.setEnabled(True)
+
+    @pyqtSlot(np.ndarray, int, int)  # 調整影像執行緒的信號槽
+    def display_process_video(self, npImg, height, width):
+        qimg = get_q_img(img=npImg, w=width, h=height)
+        self.scene_adjusting.clear()  # 動態執行影像，清理Scene
+        self.scene_adjusting.addPixmap(QPixmap(qimg))
+        self.graph_process.setScene(self.scene_adjusting)
+        self.graph_process.fitInView(self.scene_adjusting.sceneRect(), Qt.KeepAspectRatio)
+
+    # 開啟選取儲存路徑資料夾的按鈕事件
     def open_select_dir_dialog(self):
+
         self.select_dir_thread = threading.Thread(target=self.run_dir_dialog)
         self.select_dir_thread.start()
 
@@ -232,23 +233,6 @@ class CamaraCapture(QMainWindow, camera_ui.Ui_MainWindow):
         # print(self.save_path)
         if self.save_path:
             self.lineEdit.setText(self.save_path)
-
-    def open_upload_dialog(self):
-        self.open_upload_thread = threading.Thread(target=self.run_upload_dialog)
-        self.open_upload_thread.start()
-
-    # 選擇儲放影像的路徑
-    def run_upload_dialog(self):
-
-        image, _ = QFileDialog.getOpenFileName(self, '載入圖像', 'D:', "Image Files (*.jpg *.jpeg *.png)")  # 設置文件擴展名過濾,用雙分號間隔
-
-        if image:
-            self.SIGNAL_UPLOAD_IMG.emit(image)
-            self.lb_upload_path.setText(image)
-
-    @pyqtSlot(str)
-    def show_upload_img(self, upload_img):
-        self.lb_upload_path.setText(upload_img)
 
     # 判斷 line edit 是否存在字串內容
     def validate_path(self):
@@ -263,11 +247,9 @@ class CamaraCapture(QMainWindow, camera_ui.Ui_MainWindow):
         self.frame = frame
         height, width = frame.shape[:2]
         qimg = QImage(bytes(frame), width, height, 3 * width, QImage.Format_BGR888)
-        scene_origin = QGraphicsScene()
-        scene_origin.addPixmap(QPixmap(qimg))
-        self.graph_origin.setScene(scene_origin)
-        # 讓場景自適應視圖 兩個與法都可以用，第一個參數代表 場景的邊界框
-        self.graph_origin.fitInView(scene_origin.sceneRect(), Qt.KeepAspectRatio)
+        self.scene_origin.addPixmap(QPixmap(qimg))
+        self.graph_origin.setScene(self.scene_origin)
+        self.graph_origin.fitInView(self.scene_origin.sceneRect(), Qt.KeepAspectRatio)
 
     # 拍照
     def capture(self):
@@ -288,7 +270,7 @@ class CamaraCapture(QMainWindow, camera_ui.Ui_MainWindow):
     def close_sys(self):
         try:
             self.camera_thread.stop()
-            self.select_dir_thread.stop()
+            sys.exit()
         except Exception as exc:
             print(exc)
 
@@ -298,6 +280,19 @@ def set_pixels(cap):
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, WIDTH)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, HEIGHT)
     cap.set(cv2.CAP_PROP_FPS, 24)  # FPS
+
+
+# 判斷 影像為幾通道，給予對應的 qimg
+def get_q_img(img, w, h):
+    # 假如影像有處理就以 2 通道來顯示，否則就以原圖 3 通道呈現
+    if len(img.shape) == 2:
+        bytesPerline = w
+        qimg = QImage(img.data, w, h, bytesPerline, QImage.Format_Grayscale8)
+    else:
+        bytesPerline = w * 3
+        qimg = QImage(img.data, w, h, bytesPerline, QImage.Format_BGR888)
+
+    return qimg
 
 
 if __name__ == '__main__':
